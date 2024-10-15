a5166461e0bcf6ef147183f54725af05dfb89704
Kent Salarda <kentsalarda@gmail.com>
2024-10-15 10:14:43 +0800

    Usable Custom Calendar Added

---
+++ BloodSeeker/Components/Calendar/CalendarFrame.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace BloodSeeker.Components.Calendar
+{
+    partial class CalendarFrame
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.monthlbl = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(346, 84);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 17);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Sun";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(294, 84);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 17);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Sat";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(242, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Fri";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(187, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Thu";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(132, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Wed";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Mon";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(80, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Tue";
+            // 
+            // monthlbl
+            // 
+            this.monthlbl.AutoSize = true;
+            this.monthlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthlbl.Location = new System.Drawing.Point(141, 33);
+            this.monthlbl.Name = "monthlbl";
+            this.monthlbl.Size = new System.Drawing.Size(109, 24);
+            this.monthlbl.TabIndex = 11;
+            this.monthlbl.Text = "MonthLabel";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(16, 109);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(380, 332);
+            this.flowLayoutPanel1.TabIndex = 10;
+            // 
+            // CalendarFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.monthlbl);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "CalendarFrame";
+            this.Size = new System.Drawing.Size(445, 455);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label monthlbl;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ BloodSeeker/Components/Calendar/CalendarFrame.cs
@@ -0,0 +1,53 @@
+﻿using BloodSeeker.Components.Appointment_Bar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BloodSeeker.Components.Calendar
+{
+    public partial class CalendarFrame : UserControl
+    {
+        private static int _year,_month;
+        private static FlowLayoutPanel _flowLayoutPanel;
+        public CalendarFrame(FlowLayoutPanel flowpanel)
+        {
+            InitializeComponent();
+            _flowLayoutPanel = flowpanel;
+            addDay(DateTime.Now.Month, DateTime.Now.Year);
+        }
+        private void addDay(int month, int year)
+        {
+            monthlbl.Text = new DateTimeFormatInfo().GetMonthName(month).ToString() + " " + year.ToString();
+            flowLayoutPanel1.Controls.Clear();
+            _year = year;
+            _month = month;
+            DateTime start_of_month = new DateTime(year, month, 1);
+            int day = DateTime.DaysInMonth(year, month);
+            int week = Convert.ToInt32(start_of_month.DayOfWeek.ToString("d"));
+            for (int i = 1; i < week; i++)
+            {
+                CalendarButton calendarButton = new CalendarButton("");
+                flowLayoutPanel1.Controls.Add(calendarButton);
+
+            }
+            for (int i = 1; i < day; i++)
+            {
+                CalendarButton calendarButton = new CalendarButton(i + "");
+                flowLayoutPanel1.Controls.Add(calendarButton);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Appointment appointment = new Appointment();
+                _flowLayoutPanel.Controls.Add(appointment);
+            }
+        }
+    }
+}
