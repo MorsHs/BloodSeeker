5ff7fce2ec914f35bb03c6609cb5d2fdf82fe6fb
MorsHs <skyblade163@gmail.com>
2024-10-19 21:54:15 +0800

    fuck this retarded IDE

--- BloodSeeker/Form1.cs
+++ BloodSeeker/Form1.cs
@@ -1,6 +1,5 @@
 ﻿using BloodSeeker.Admin;
 using BloodSeeker.Client;
-using Guna.UI2.AnimatorNS;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,41 +16,87 @@ namespace BloodSeeker
     {
         AdminLogin adminlog = new AdminLogin();
         ClienntLogin clientlog = new ClienntLogin();
-        SignupController controller;
-        Global globe;
+        SignUpController controller;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        public String checkboxChecker()
+        {
+            if (malecmbx.Checked) return "Male";
+            else return "Female";
+        }
+
         private void btn_signUp_Click(object sender, EventArgs e)
         {
-           controller = new SignupController(txt_lastname.Text.ToString(),txt_firstname.Text.ToString(),txt_contact.Text.ToString(),
-               txt_email.Text.ToString(),txt_pass.Text.ToString(),txt_confirmpass.Text.ToString());
+            //Evil IF Nesting incoming
+            if (txt_pass.Text.ToString() == txt_confirmpass.Text.ToString())
+            {
+                if (txt_firstname.Text != "" &&
+                    txt_lastname.Text != "" &&
+                    txt_username.Text != "" &&
+                    txt_pass.Text != "" &&
+                    txt_confirmpass.Text != "" &&
+                    txt_email.Text != "" &&
+                    txt_address.Text != "" &&
+                    malecmbx.Checked || femalecmbx.Checked
+                    )
+                {
+                    if (pickerDate.Value.Date <= DateTime.Now.Date)
+                    {
+                        controller = new SignUpController(txt_firstname.Text.ToString(), txt_lastname.Text.ToString(),
+                txt_pass.Text.ToString(), txt_username.Text.ToString(), txt_email.Text.ToString(),
+                txt_contact.Text.ToString(), txt_address.Text.ToString(), checkboxChecker(), pickerDate.Value.Date);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Today is " + DateTime.Now.Date + " its not possible to put " + pickerDate.Value.Date);
+                        pickerDate.Value = DateTime.Now.Date;
+                    }
+
+                }
+                else MessageBox.Show("Please Fill out the missing forms");
+            }
+            else
+            {
+                MessageBox.Show("Password and Confirm Password did not match.");
+                txt_confirmpass.Text = txt_pass.Text = "";
+            }
+
         }
 
         private void link_login_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             cmb_option.Show();
-            
+
+        }
+
+        private void guna2ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            globe = new Global();
-            globe.fncConnectToDatabase();
             cmb_option.Hide();
         }
 
         private void cmb_option_SelectedIndexChanged(object sender, EventArgs e)
         {
-          
-            if(cmb_option.Text == "Admin")
+
+            if (cmb_option.Text == "Admin")
             {
                 adminlog.Show();
                 this.Hide();
             }
-            else if(cmb_option.Text == "Client")
+            else if (cmb_option.Text == "Client")
             {
                 clientlog.Show();
                 this.Hide();
@@ -66,6 +111,22 @@ namespace BloodSeeker
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void malecmbx_CheckedChanged(object sender, EventArgs e)
+        {
+            if (malecmbx.Checked)
+            {
+                femalecmbx.Checked = false;
+            }
+        }
+
+        private void femalecmbx_CheckedChanged(object sender, EventArgs e)
+        {
+            if (femalecmbx.Checked)
+            {
+                malecmbx.Checked = false;
+            }
         }
     }
 }
--- BloodSeeker/SignupController.cs
+++ BloodSeeker/SignupController.cs
@@ -1,30 +1,59 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace BloodSeeker
 {
-    internal class SignupController
-
+    internal class SignUpController
     {
-        private String lastname;
-        private String firstname;
-        private String contactnum;
-        private String email;
-        private String password;
-        private String confirmpassword;
+        private string username { get; set; }
+        private string password { get; set; }
+        private string firstname { get; set; }
+        private string lastname { get; set; }
+        private string email { get; set; }
+        private string phone { get; set; }
+        private string address { get; set; }
+        private string sex { get; set; }
+        private DateTime birthdate { get; set; }
 
-        Global globe;
-       public SignupController(string lastname, string firstname, string contactum,string email,string password,string confirmpassword) { 
-            this.lastname = lastname;
-            this.firstname = firstname;
-            this.contactnum = contactum;
-            this.email = email;
+        public SignUpController(string firstname, string lastname, string password, string username, string email, string phone,
+            string address, string sex, DateTime birthdate)
+        {
+            this.username = username;
             this.password = password;
-            this.confirmpassword = confirmpassword;
-            globe = new Global();
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.email = email;
+            this.phone = phone;
+            this.address = address;
+            this.sex = sex;
+            this.birthdate = birthdate;
+            registerUser();
+        }
+
+        public void registerUser()
+        {
+            Global global = new Global();
+            global.fncConnectToDatabase();
+            global.sqlCommand.Parameters.Clear();
+            //Dissapointing na dle nako makuha pa ang clear ug command text into one method kay wla koy time and lazy to do
+            global.sqlCommand.CommandText = "prc_createAccountForClient";
+            global.sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            global.sqlCommand.Parameters.AddWithValue("p_first_name", firstname);
+            global.sqlCommand.Parameters.AddWithValue("p_last_name", lastname);
+            global.sqlCommand.Parameters.AddWithValue("p_address", address);
+            global.sqlCommand.Parameters.AddWithValue("p_mobileNum", phone);
+            global.sqlCommand.Parameters.AddWithValue("p_emailAdd", email);
+            global.sqlCommand.Parameters.AddWithValue("p_birthDate", birthdate);
+            global.sqlCommand.Parameters.AddWithValue("p_sex", sex);
+            global.sqlCommand.Parameters.AddWithValue("p_username", username);
+            global.sqlCommand.Parameters.AddWithValue("p_password", password);
+            global.sqlCommand.ExecuteNonQuery();
         }
     }
 }
