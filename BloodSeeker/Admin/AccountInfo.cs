e79b42007fdc0dd0745091a02d5455f8cd23e344
BreadWithJam <lubayjamyca@gmail.com>
2024-10-23 16:06:07 +0800

    admin settings

--- BloodSeeker/Admin/AccountInfo.cs
+++ BloodSeeker/Admin/AccountInfo.cs
@@ -43,5 +43,70 @@ namespace BloodSeeker.Admin
         {
             txt_username.Enabled = true;
         }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            txt_firstname.Enabled = true;
+        }
+
+        private void btn_editUsername_Click(object sender, EventArgs e)
+        {
+            txt_username.Enabled = true;
+        }
+
+        private void btn_editLN_Click(object sender, EventArgs e)
+        {
+            txt_lastname.Enabled = true;
+        }
+
+        private void btn_editAddress_Click(object sender, EventArgs e)
+        {
+            txt_address.Enabled = true;
+        }
+
+        private void btn_editCN_Click(object sender, EventArgs e)
+        {
+            txt_contactNo.Enabled = true;
+        }
+
+        private void btn_editEA_Click(object sender, EventArgs e)
+        {
+            txt_email.Enabled = true;
+        }
+        private void showButton()
+        {
+            btn_cancel.Show();
+            btn_Submit.Show();
+        }
+
+        private void txt_username_TextChanged_1(object sender, EventArgs e)
+        {
+            showButton();
+        }
+
+        private void txt_firstname_TextChanged(object sender, EventArgs e)
+        {
+            showButton();
+        }
+
+        private void txt_lastname_TextChanged(object sender, EventArgs e)
+        {
+            showButton();
+        }
+
+        private void txt_address_TextChanged(object sender, EventArgs e)
+        {
+            showButton();
+        }
+
+        private void txt_contactNo_TextChanged(object sender, EventArgs e)
+        {
+            showButton();
+        }
+
+        private void txt_email_TextChanged(object sender, EventArgs e)
+        {
+            showButton();
+        }
     }
 }
