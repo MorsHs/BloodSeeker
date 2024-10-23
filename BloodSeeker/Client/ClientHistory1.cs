d79efc1d4ffcb92fc68ce27c7dee345a66fde0d8
gian <babagian24@gmail.com>
2024-10-23 20:36:45 +0800

    Added History, Inventory and Settings in Client

---
+++ BloodSeeker/Client/ClientHistory1.cs
@@ -0,0 +1,27 @@
+﻿using BloodSeeker.Components.Calendar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BloodSeeker.Client
+{
+    public partial class ClientHistory1 : Form
+    {
+        public ClientHistory1()
+        {
+            InitializeComponent();
+        }
+
+        private void ClientHistory1_Load(object sender, EventArgs e)
+        {
+            CalendarFrame calendarFrame = new CalendarFrame(flowPanel1);
+            flowPanel1.Controls.Add(calendarFrame);
+        }
+    }
+}
