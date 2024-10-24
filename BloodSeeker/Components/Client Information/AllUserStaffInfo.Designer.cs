f628a1e9803946a403792d589582e8bf94e97788
MorsHs <skyblade163@gmail.com>
2024-10-24 12:20:13 +0800

    View More Staff add

---
+++ BloodSeeker/Components/Client Information/AllUserStaffInfo.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace BloodSeeker.Components.Client_Information
+{
+    partial class AllUserStaffInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_lastname = new System.Windows.Forms.Label();
+            this.txt_firstname = new System.Windows.Forms.Label();
+            this.txt_phone = new System.Windows.Forms.Label();
+            this.txt_address = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.Label();
+            this.txt_sex = new System.Windows.Forms.Label();
+            this.txt_birthdate = new System.Windows.Forms.Label();
+            this.guna2Separator1 = new Guna.UI2.WinForms.Guna2Separator();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2CirclePictureBox1
+            // 
+            this.guna2CirclePictureBox1.ImageRotate = 0F;
+            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(149, 25);
+            this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(116, 112);
+            this.guna2CirclePictureBox1.TabIndex = 0;
+            this.guna2CirclePictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(58, 511);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Birth Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 356);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(26, 300);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Contact Number:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(70, 405);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 25);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(70, 458);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 25);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Sex";
+            // 
+            // txt_lastname
+            // 
+            this.txt_lastname.AutoSize = true;
+            this.txt_lastname.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_lastname.Location = new System.Drawing.Point(216, 238);
+            this.txt_lastname.Name = "txt_lastname";
+            this.txt_lastname.Size = new System.Drawing.Size(149, 25);
+            this.txt_lastname.TabIndex = 8;
+            this.txt_lastname.Text = "Client Lastname";
+            // 
+            // txt_firstname
+            // 
+            this.txt_firstname.AutoSize = true;
+            this.txt_firstname.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_firstname.Location = new System.Drawing.Point(216, 190);
+            this.txt_firstname.Name = "txt_firstname";
+            this.txt_firstname.Size = new System.Drawing.Size(119, 25);
+            this.txt_firstname.TabIndex = 9;
+            this.txt_firstname.Text = "Client Name";
+            // 
+            // txt_phone
+            // 
+            this.txt_phone.AutoSize = true;
+            this.txt_phone.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_phone.Location = new System.Drawing.Point(216, 300);
+            this.txt_phone.Name = "txt_phone";
+            this.txt_phone.Size = new System.Drawing.Size(138, 25);
+            this.txt_phone.TabIndex = 10;
+            this.txt_phone.Text = "Client Number";
+            // 
+            // txt_address
+            // 
+            this.txt_address.AutoSize = true;
+            this.txt_address.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_address.Location = new System.Drawing.Point(216, 356);
+            this.txt_address.Name = "txt_address";
+            this.txt_address.Size = new System.Drawing.Size(135, 25);
+            this.txt_address.TabIndex = 11;
+            this.txt_address.Text = "Client Address";
+            // 
+            // txt_email
+            // 
+            this.txt_email.AutoSize = true;
+            this.txt_email.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_email.Location = new System.Drawing.Point(216, 405);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(114, 25);
+            this.txt_email.TabIndex = 12;
+            this.txt_email.Text = "Client Email";
+            // 
+            // txt_sex
+            // 
+            this.txt_sex.AutoSize = true;
+            this.txt_sex.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_sex.Location = new System.Drawing.Point(216, 458);
+            this.txt_sex.Name = "txt_sex";
+            this.txt_sex.Size = new System.Drawing.Size(97, 25);
+            this.txt_sex.TabIndex = 13;
+            this.txt_sex.Text = "Client Sex";
+            // 
+            // txt_birthdate
+            // 
+            this.txt_birthdate.AutoSize = true;
+            this.txt_birthdate.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_birthdate.Location = new System.Drawing.Point(216, 511);
+            this.txt_birthdate.Name = "txt_birthdate";
+            this.txt_birthdate.Size = new System.Drawing.Size(147, 25);
+            this.txt_birthdate.TabIndex = 14;
+            this.txt_birthdate.Text = "Client Birthdate";
+            // 
+            // guna2Separator1
+            // 
+            this.guna2Separator1.Location = new System.Drawing.Point(-1, 153);
+            this.guna2Separator1.Name = "guna2Separator1";
+            this.guna2Separator1.Size = new System.Drawing.Size(414, 19);
+            this.guna2Separator1.TabIndex = 15;
+            // 
+            // AllUserStaffInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(412, 608);
+            this.Controls.Add(this.guna2Separator1);
+            this.Controls.Add(this.txt_birthdate);
+            this.Controls.Add(this.txt_sex);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.txt_address);
+            this.Controls.Add(this.txt_phone);
+            this.Controls.Add(this.txt_firstname);
+            this.Controls.Add(this.txt_lastname);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2CirclePictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AllUserStaffInfo";
+            this.Text = "AllUserStaffInfo";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.AllUserStaffInfo_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label txt_lastname;
+        private System.Windows.Forms.Label txt_firstname;
+        private System.Windows.Forms.Label txt_phone;
+        private System.Windows.Forms.Label txt_address;
+        private System.Windows.Forms.Label txt_email;
+        private System.Windows.Forms.Label txt_sex;
+        private System.Windows.Forms.Label txt_birthdate;
+        private Guna.UI2.WinForms.Guna2Separator guna2Separator1;
+    }
+}
