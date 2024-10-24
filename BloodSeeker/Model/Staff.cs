e8b1aa329aafea8c0d3101acaed478a5307958f1
MorsHs <skyblade163@gmail.com>
2024-10-24 14:35:50 +0800

    Class Create

---
+++ BloodSeeker/Model/Staff.cs
@@ -0,0 +1,19 @@
+﻿using BloodSeeker.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodSeeker.Model
+{
+    internal class Staff:Person
+    {
+        public int _id { get;,set; }
+        public Staff(string username,string password,string firstname,string lastname,string email,string phone,string address,string sex,DateTime birthdate,int id):
+            base(username,password,firstname,lastname,email,phone,address,sex,birthdate)
+        {
+            _id = id;
+        }
+    }
+}
